953130492657cdb3dbed872818f13056eb294ee1
brandonk8325 <brandonkerpel@gmail.com>
2024-09-17 10:10:12 -0500

    test

--- src/Components/Header/HeaderElements.js
+++ src/Components/Header/HeaderElements.js
@@ -11,9 +11,9 @@ export const HeaderLink = styled(Link)`
     textAlign: center;
     color: #1e245c;
     text-decoration: none;
-    font-size: 4.4vh;
+    font-size: 40px;
     font-weight: bold;
-    padding-top: 4.4vh;
+    padding-top: 40px;
     
 `;
 
